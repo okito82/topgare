9aa790dba537dff227a6fa4f4292a40211d54838
Okito <okito82@hotmail.com>
2020-02-25 23:37:57 +0100

    refonte projet

---
+++ src/main/scala/org/BanqueDataFrame.scala
@@ -0,0 +1,66 @@
+package org
+
+import org.apache.spark.sql.types.{DateType, IntegerType, StringType, StructField, StructType}
+import org.banque.sparkSession
+
+object BanqueDataFrame {
+
+  val clientSchema = StructType(
+    Array(
+      StructField("no_client", IntegerType, nullable = true),
+      StructField("nom", StringType, nullable = true),
+      StructField("email", StringType, nullable = true),
+      StructField("commentaire", StringType, nullable = true)
+    )
+  )
+
+  val clientDF = sparkSession.read.format("csv")
+    .option("sep", ",")
+    .option("inferSchema", "true")
+    .schema(clientSchema)
+    .load("src/main/resources/client.csv")
+
+  val comptetSchema = StructType(
+    Array(
+      StructField("no_compte", IntegerType, nullable = true),
+      StructField("solde", IntegerType, nullable = true),
+      StructField("no_client", IntegerType, nullable = true)
+    )
+  )
+
+
+  val compteDF = sparkSession.read.format("csv")
+    .option("sep", ",")
+    .option("inferSchema", "true")
+    .schema(comptetSchema)
+    .load("src/main/resources/compte.csv")
+
+  val commercialSchema = StructType(
+    Array(
+      StructField("no_commercial", IntegerType, nullable = true),
+      StructField("nom", StringType, nullable = true)
+    )
+  )
+
+  val commercialDF = sparkSession.read.format("csv")
+    .option("sep", ",")
+    .option("inferSchema", "true")
+    .schema(commercialSchema)
+    .load("src/main/resources/commercial.csv")
+
+  val portfeuilleSchema = StructType (
+    Array(
+      StructField("no_commercial", IntegerType, nullable = true),
+      StructField("no_client", IntegerType, nullable = true),
+      StructField("Date", DateType, nullable = true)
+    )
+  )
+
+  val portefeuilleDF = sparkSession.read.format("csv")
+    .option("sep", ",")
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .load("src/main/resources/portefeuille.csv")
+
+
+}
---
+++ src/main/scala/org/banque.scala
@@ -0,0 +1,41 @@
+package org
+
+import org.apache.log4j.LogManager
+import org.BanqueDataFrame._
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions._
+
+import scala.reflect.macros.whitebox
+
+
+/**
+ * Hello world!
+ *
+ */
+object banque  extends App {
+
+    val logger = LogManager.getLogger("AnalystTopGare*********************************************************")
+    val sparkSession = ApplicationContext.getSparkSession("TopGare")
+
+
+  /* Nom et mel de tous les clients */
+  //clientDF.select("nom","email").show()
+
+  /* Date d'attribution sans doublon */
+  //portefeuilleDF.select("no_client").show()
+
+  /* Longueur du email des clients (fonction chaine) */
+  //clientDF.select(length(col("email"))).show()
+
+  clientDF.withColumn("nn",when(col("no_client") === 1,1).otherwise(0)).show()
+
+
+
+
+
+
+
+
+
+
+}
